4705f5f979444129e3f6b26a68eb62193f1dd184
Kishan Mishra <mishra.kishan1@gmail.com>
2024-06-16 11:57:48 +0400

    fix: align resume with links for smaller devices

--- src/sections/About/SocialLinks.jsx
+++ src/sections/About/SocialLinks.jsx
@@ -4,24 +4,40 @@ import { useTheme } from "../../common/ThemeContext";
 
 const SocialLinks = () => {
   const { theme } = useTheme();
+  const links = socialLinks(theme);
+
   return (
     <div className="flex flex-wrap gap-5 sm:flex-row items-end">
       <div className="flex flex-wrap gap-5 sm:flex-row mt-4">
-        {socialLinks(theme).map((item, index) => (
-          <a
-            className="scaleOnHover flex flex-col justify-center items-center"
-            key={index}
-            href={item.link}
-            target="_blank"
-          >
-            <img height={42} width={42} src={item.iconUrl} />
-          </a>
-        ))}
-      </div>
-      <div>
-        <a href={CV} download>
-          <button className="hover"> Resume </button>
-        </a>
+        {links.map((item, index) => {
+          if (index === links.length - 1) {
+            return (
+              <>
+                <a
+                  className="scaleOnHover flex flex-col justify-center items-center"
+                  key={index}
+                  href={item.link}
+                  target="_blank"
+                >
+                  <img height={42} width={42} src={item.iconUrl} />
+                </a>
+                <a href={CV} download>
+                  <button className="hover"> Resume </button>
+                </a>
+              </>
+            );
+          }
+          return (
+            <a
+              className="scaleOnHover flex flex-col justify-center items-center"
+              key={index}
+              href={item.link}
+              target="_blank"
+            >
+              <img height={42} width={42} src={item.iconUrl} />
+            </a>
+          );
+        })}
       </div>
     </div>
   );
